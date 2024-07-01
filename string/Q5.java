90b0c1c2cfcc6a230491de2aafd94a08da10ae0a
bion0625 <bion0625@gmail.com>
2024-07-01 19:53:19 +0900

    chapter4 문자열 - 문제 7 이상한 문자 만들기

--- string/Q5.java
+++ string/Q5.java
@@ -1,4 +1,4 @@
-package array;
+package string;
 
 public class Q5 {
     public static void main(String[] args) {
--- string/Q6.java
+++ string/Q6.java
@@ -1,4 +1,4 @@
-package array;
+package string;
 
 public class Q6 {
     public static void main(String[] args) {
