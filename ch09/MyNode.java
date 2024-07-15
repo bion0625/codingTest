222e21e8f07001578edd76b3d19e2bf4484aefd3
이의정 <stxtory@gmail.com>
2024-07-15 21:08:06 +0900

    문제 20 유효한 괄호

---
+++ ch09/MyNode.java
@@ -0,0 +1,12 @@
+package ch09;
+
+public class MyNode {
+    int item;
+    // 노드의 다음 노드
+    MyNode next;
+
+    public MyNode(int item, MyNode next) {
+        this.item = item;
+        this.next = next;
+    }
+}
---
+++ ch09/Test.java
@@ -0,0 +1,33 @@
+package ch09;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(stack.pop());
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println(queue.poll());
+
+        Deque<Integer> dequeStack = new ArrayDeque<>();
+        dequeStack.push(1);
+        dequeStack.push(2);
+        dequeStack.push(3);
+        System.out.println(dequeStack.pop());
+    }
+}
