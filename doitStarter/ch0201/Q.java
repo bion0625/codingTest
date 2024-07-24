7dac16afd78fe482caad716052bbde758d6939ab
이의정 <stxtory@gmail.com>
2024-07-24 17:00:54 +0900

    02-1 배열이란? - Q6

--- doitStarter/ch0201/Q.java
+++ doitStarter/ch0201/Q.java
@@ -48,6 +48,9 @@ public class Q {
         k = new int[x.length];
         rcopy(k, x);
         System.out.println(Arrays.toString(k));
+
+        // Q6
+        q6();
     }
 
     static int maxOf(int[] a) {
@@ -120,5 +123,59 @@ public class Q {
     static void rcopy(int[] a, int[] b) {
         for (int i = 0; i < b.length; i++)
             a[b.length - i - 1] = b[i];
+    }
+
+    static int cardConv(int x, int r, char[] d) {
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do {
+            System.out.print(r + "|   " + x);
+            d[digits] = dchar.charAt(x % r);
+            System.out.println("..." + d[digits]);
+            System.out.println(" +-----");
+            digits++;
+            x /= r;
+        } while (x != 0);
+
+        for (int i = 0; i < digits / 2; i++) {
+            char t = d[i];
+            d[i] = d[digits - i - 1];
+            d[digits - i - 1] = t;
+        }
+
+        return digits;
+    }
+
+    static void q6() {
+        Scanner stdIn = new Scanner(System.in);
+        int no;
+        int cd;
+        int dno;
+        int retry;
+        char[] cno = new char[32];
+
+        System.out.println("10진수를 기수 변환합니다.");
+        do {
+            do {
+                System.out.print("변환하는 음이 아닌 정수: ");
+                no = stdIn.nextInt();
+            } while (no < 0);
+
+            do {
+                System.out.print("어떤 진수로 변환할까요? (2~36): ");
+                cd = stdIn.nextInt();
+            } while (cd < 2 || cd > 36);
+
+            dno = cardConv(no, cd, cno);
+
+            System.out.print(cd + "진수로 ");
+            for (int i = 0; i < dno; i++)
+                System.out.print(cno[i]);
+            System.out.println("입니다.");
+
+            System.out.print("한 번 더 할까요? (1_예/0_아니요): ");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
     }
 }
