0fe9501072b5a067395e8105b99a90ebdf27f920
이의정 <stxtory@gmail.com>
2024-08-29 20:26:34 +0900

    07-2 KMP법

---
+++ doitStarter/ch0702/Q.java
@@ -0,0 +1,75 @@
+package doitStarter.ch0702;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q {
+
+    static int kmpMatch(String txt, String pat) {
+        int pt = 1;
+        int pp = 0;
+        int[] skip = new int[pat.length() + 1]; // 건너뛰기 표
+
+        //건너뛰기 표 만들기
+        skip[pt] = 0;
+        while (pt != pat.length()) {
+            if (pat.charAt(pt) == pat.charAt(pp))
+                skip[++pt] = ++pp;
+            else if (pp == 0)
+                skip[++pt] = pp;
+            else
+                pp = skip[pp];
+        }
+        System.out.println("skip: " + Arrays.toString(skip));
+
+        // 검색
+        pt = pp = 0;
+        while (pt != txt.length() && pp != pat.length()) {
+            System.out.println(txt);
+            System.out.printf(String.format("%%%ds\n", pt + 1), (txt.charAt(pt) == pat.charAt(pp)) ? "+" : "|");
+            System.out.printf(String.format("%%%ds\n", pt - pp + pat.length()), pat);
+            System.out.println();
+            if (txt.charAt(pt) == pat.charAt(pp)) {
+                pt++;
+                pp++;
+            } else if (pp == 0)
+                pt++;
+            else
+                pp = skip[pp];
+        }
+
+        if (pp == pat.length())
+            return pt - pp;
+        return -1;
+    }
+
+    static void q3() {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("텍스트: ");
+        String s1 = stdIn.next();
+
+        System.out.print("패 턴: ");
+        String s2 = stdIn.next();
+
+        int idx = kmpMatch(s1, s2);
+
+        if (idx == -1)
+            System.out.println("텍스트에 패턴이 없습니다.");
+        else {
+            // 일치하는 문자 바로 앞까지의 문자 개수를 반각 문자로 환산하여 구함
+            int len = 0;
+            for (int i = 0; i < idx; i++)
+                len += s1.substring(i, i + 1).getBytes().length;
+            len += s2.length();
+
+            System.out.println((idx + 1) + "번째 문자부터 일치합니다.");
+            System.out.println("텍스트: " + s1);
+            System.out.printf(String.format("패 턴: %%%ds\n", len), s2);
+        }
+    }
+
+    public static void main(String[] args) {
+        q3();
+    }
+}
